df8f16885d2ae5c2f014ed8bec6df12c1357258e
reckonzhang <zhang_5521@126.com>
2023-04-26 00:45:02 +0800

    add

--- styles/index.js
+++ styles/index.js
@@ -8,7 +8,7 @@ export default function Home() {
       <Head>
         <title>Colnago</title>
         <meta name="description" content="write-hub" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/G.png" />
       </Head>
 
       <main className={styles.main}>
